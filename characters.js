915d436fbf301129844d2672ea0f6fef6b798778
erick112x <110348023+erick112x@users.noreply.github.com>
2022-11-30 23:47:56 -0300

    Add files via upload

---
+++ characters.js
@@ -0,0 +1,330 @@
+
+
+var bob = {
+        nome: "Bob Esponja Calça Quadrada",
+        genero: "Masculino",
+        especie: "Esponja-do-mar",
+        profissao: "Mestre-cuca",
+        posicao: "1°"
+      };
+
+var patrick = {
+        nome: "Patrick Estrela",
+        genero: "Masculino",
+        especie: "Estrela do mar",
+        profissao: "Desempregado",
+        posicao: "2°"
+      };
+
+var sandy = {
+        nome: "Sandy Bochechas",
+        genero: "Feminino",
+        especie: "Esquilo",
+        profissao: "Cientista",
+        posicao: "3°"
+      };
+
+var seuSiriguejo = {
+        nome: "Eugene Hood Sirigueijo",
+        genero: "Masculino",
+        especie: "Caranguejo",
+        profissao: "Gerente do Siri Cascudo",
+        posicao: "4°"
+      };
+
+var perolaSiriguejo=  {
+        nome: "Pérola Sirigueijo",
+        genero: "Feminino",
+        especie: "Baleia",
+        profissao: "Garota Programa do Siri Cascudo",
+        posicao: "5°"
+      };
+
+var lulaMolusco = {
+        nome: "Lula Molusco Tentaculos",
+        genero: "Masculino",
+        especie: "Lula",
+        profissao: "Caixa do Siri Cascudo",
+        posicao: "6°"
+      };
+
+var garyCaracol = {
+        nome: "Gary Caracol",
+        genero: "Masculino",
+        especie: "Caramujo",
+        profissao: "Desempregado",
+        posicao: "7°"
+      };
+
+var plankton = {
+        nome: "Sheldon J. Plankton",
+        genero: "Masculino",
+        especie: "Plâncton",
+        profissao: "Fundador do Balde de Lixo",
+        posicao: "8°"
+      };
+
+var sraPuff = {
+        nome: "Senhora Puff",
+        genero: "Feminino",
+        especie: "Baiacu",
+        profissao: "Professora",
+        posicao: "9°"
+      };
+
+var homemSereia = {
+        nome: "Homem-Sereia",
+        genero: "Masculino",
+        especie: "Homem",
+        profissao: "Herói",
+        posicao: "10°"
+      };
+
+var mexilhaozinho = {
+        nome: "Mexilhãozinho",
+        genero: "Masculino",
+        especie: "Homem",
+        profissao: "Herói",
+        posicao: "11°"
+      };
+
+var larryALagosta = {
+        nome: "Larry, A Lagosta",
+        genero: "Masculino",
+        especie: "Lagosta",
+        profissao: "Salva-vidas",
+        posicao: "12°"
+      };
+
+var perch = {
+        nome: "Perch Perkins",
+        genero: "Masculino",
+        especie: "Peixe",
+        profissao: "Âncora do jornal da Fenda Do Biquini",
+        posicao: "13°"
+      };
+
+var princesa = {
+        nome: "Princesa Mindy",
+        genero: "Feminino",
+        especie: "Sereia",
+        profissao: "Desempregada",
+        posicao: "14°"
+};
+
+var patchy = {
+        nome: "Patchy O Pirata",
+        genero: "Masculinho",
+        especie: "Homem",
+        profissao: "Presidente do fã-clube do Bob Esponja",
+        posicao: "15°"
+};
+
+var potty = {
+        nome: "Potty o Papagaio",
+        genero: "Masculinho",
+        especie: "Homem",
+        profissao: "Presidente do fã-clube do Bob Esponja",
+        posicao: "16°",
+};
+
+var karen = {
+      nome: "Karen Plankton",
+      genero: "Feminino",
+      especie: "Computador",
+      profissao: "Co-fundadora Do Balde de Lixo",
+      posicao: "17°",
+};
+
+var fancyson = {
+      nome: "Squilliam Fancyson",
+      genero: "Masculino",
+      especie: "Lula",
+      profissao: "Desempregado",
+      posicao: "18°",
+};
+
+var margaret = {
+     nome: "Margaret Calça Quadrada",
+     genero: "Feminino",
+     especie: "Esponja",
+     profissao: "Aposentada",
+     posicao: "19°",
+};
+
+var harold = {
+    nome: "Harold Calça Quadrada",
+    genero: "Masculino",
+    especie: "Esponja",
+    profissao: "Aposentado",
+    posicao: "20°",
+};
+
+var rei = {
+      nome: "Rei Neturno",
+      genero: "Masculino",
+      especie: "Sereio",
+      profissao: "Rei",
+      posicao: "21°",
+};
+
+var raio = {
+    nome: "Homem Raio",
+    genero: "Masculino",
+    especie: "Desconhecida",
+    profissao: "Vilão",
+    posicao: "22°",
+}
+
+
+document.getElementById("bob-nome").innerHTML = "" + bob.nome + "<br>";
+document.getElementById("bob-genero").innerHTML = "Genero : " + bob.genero + "<br>";
+document.getElementById("bob-especie").innerHTML = "Espécie : " + bob.especie + "<br>";
+document.getElementById("bob-profissao").innerHTML = "Profissão : " + bob.profissao + "<br>";
+document.getElementById("bob-posicao").innerHTML = "Posição de aparições : " + bob.posicao + "<br>";
+
+
+document.getElementById("patrick-nome").innerHTML = "" + patrick.nome + "<br>";
+document.getElementById("patrick-genero").innerHTML = "Genero : " + patrick.genero + "<br>";
+document.getElementById("patrick-especie").innerHTML = "Espécie : " + patrick.especie + "<br>";
+document.getElementById("patrick-profissao").innerHTML = "Profissão : " + patrick.profissao + "<br>";
+document.getElementById("patrick-posicao").innerHTML = "Posição de aparições : " + patrick.posicao + "<br>";
+
+
+document.getElementById("sandy-nome").innerHTML = "" + sandy.nome + "<br>";
+document.getElementById("sandy-genero").innerHTML = "Genero : " + sandy.genero + "<br>";
+document.getElementById("sandy-especie").innerHTML = "Espécie : " + sandy.especie + "<br>";
+document.getElementById("sandy-profissao").innerHTML = "Profissão : " + sandy.profissao + "<br>";
+document.getElementById("sandy-posicao").innerHTML = "Posição de aparições : " + sandy.posicao + "<br>";
+
+
+document.getElementById("seuSiriguejo-nome").innerHTML = "" + seuSiriguejo.nome + "<br>";
+document.getElementById("seuSiriguejo-genero").innerHTML = "Genero : " + seuSiriguejo.genero + "<br>";
+document.getElementById("seuSiriguejo-especie").innerHTML = "Espécie : " + seuSiriguejo.especie + "<br>";
+document.getElementById("seuSiriguejo-profissao").innerHTML = "Profissão : " + seuSiriguejo.profissao + "<br>";
+document.getElementById("seuSiriguejo-posicao").innerHTML = "Posição de aparições : " + seuSiriguejo.posicao + "<br>";
+
+
+document.getElementById("perolaSiriguejo-nome").innerHTML = "" + perolaSiriguejo.nome + "<br>";
+document.getElementById("perolaSiriguejo-genero").innerHTML = "Genero : " + perolaSiriguejo.genero + "<br>";
+document.getElementById("perolaSiriguejo-especie").innerHTML = "Espécie : " + perolaSiriguejo.especie + "<br>";
+document.getElementById("perolaSiriguejo-profissao").innerHTML = "Profissão : " + perolaSiriguejo.profissao + "<br>";
+document.getElementById("perolaSiriguejo-posicao").innerHTML = "Posição de aparições : " + perolaSiriguejo.posicao + "<br>";
+
+
+document.getElementById("lulaMolusco-nome").innerHTML = "" + lulaMolusco.nome + "<br>";
+document.getElementById("lulaMolusco-genero").innerHTML = "Genero : " + lulaMolusco.genero + "<br>";
+document.getElementById("lulaMolusco-especie").innerHTML = "Espécie : " + lulaMolusco.especie + "<br>";
+document.getElementById("lulaMolusco-profissao").innerHTML = "Profissão : " + lulaMolusco.profissao + "<br>";
+document.getElementById("lulaMolusco-posicao").innerHTML = "Posição de aparições : " + lulaMolusco.posicao + "<br>";
+
+
+document.getElementById("garyCaracol-nome").innerHTML = "" + garyCaracol.nome + "<br>";
+document.getElementById("garyCaracol-genero").innerHTML = "Genero : " + garyCaracol.genero + "<br>";
+document.getElementById("garyCaracol-especie").innerHTML = "Espécie : " + garyCaracol.especie + "<br>";
+document.getElementById("garyCaracol-profissao").innerHTML = "Profissão : " + garyCaracol.profissao + "<br>";
+document.getElementById("garyCaracol-posicao").innerHTML = "Posição de aparições : " + garyCaracol.posicao + "<br>";
+
+
+document.getElementById("plankton-nome").innerHTML = "" + plankton.nome + "<br>";
+document.getElementById("plankton-genero").innerHTML = "Genero : " + plankton.genero + "<br>";
+document.getElementById("plankton-especie").innerHTML = "Espécie : " + plankton.especie + "<br>";
+document.getElementById("plankton-profissao").innerHTML = "Profissão : " + plankton.profissao + "<br>";
+document.getElementById("plankton-posicao").innerHTML = "Posição de aparições : " + plankton.posicao + "<br>";
+
+
+document.getElementById("sraPuff-nome").innerHTML = "" + sraPuff.nome + "<br>";
+document.getElementById("sraPuff-genero").innerHTML = "Genero : " + sraPuff.genero + "<br>";
+document.getElementById("sraPuff-especie").innerHTML = "Espécie : " + sraPuff.especie + "<br>";
+document.getElementById("sraPuff-profissao").innerHTML = "Profissão : " + sraPuff.profissao + "<br>";
+document.getElementById("sraPuff-posicao").innerHTML = "Posição de aparições : " + sraPuff.posicao + "<br>";
+
+
+document.getElementById("homemSereia-nome").innerHTML = "" + homemSereia.nome + "<br>";
+document.getElementById("homemSereia-genero").innerHTML = "Genero : " + homemSereia.genero + "<br>";
+document.getElementById("homemSereia-especie").innerHTML = "Espécie : " + homemSereia.especie + "<br>";
+document.getElementById("homemSereia-profissao").innerHTML = "Profissão : " + homemSereia.profissao + "<br>";
+document.getElementById("homemSereia-posicao").innerHTML = "Posição de aparições : " + homemSereia.posicao + "<br>";
+
+
+document.getElementById("mexilhaozinho-nome").innerHTML = "" + mexilhaozinho.nome + "<br>";
+document.getElementById("mexilhaozinho-genero").innerHTML = "Genero : " + mexilhaozinho.genero + "<br>";
+document.getElementById("mexilhaozinho-especie").innerHTML = "Espécie : " + mexilhaozinho.especie + "<br>";
+document.getElementById("mexilhaozinho-profissao").innerHTML = "Profissão : " + mexilhaozinho.profissao + "<br>";
+document.getElementById("mexilhaozinho-posicao").innerHTML = "Posição de aparições : " + mexilhaozinho.posicao + "<br>";
+
+
+document.getElementById("larryALagosta-nome").innerHTML = "" + larryALagosta.nome + "<br>";
+document.getElementById("larryALagosta-genero").innerHTML = "Genero : " + larryALagosta.genero + "<br>";
+document.getElementById("larryALagosta-especie").innerHTML = "Espécie : " + larryALagosta.especie + "<br>";
+document.getElementById("larryALagosta-profissao").innerHTML = "Profissão : " + larryALagosta.profissao + "<br>";
+document.getElementById("larryALagosta-posicao").innerHTML = "Posição de aparições : " + larryALagosta.posicao + "<br>";
+
+
+document.getElementById("perch-nome").innerHTML = "" + perch.nome + "<br>";
+document.getElementById("perch-genero").innerHTML = "Genero : " + perch.genero + "<br>";
+document.getElementById("perch-especie").innerHTML = "Espécie : " + perch.especie + "<br>";
+document.getElementById("perch-profissao").innerHTML = "Profissão : " + perch.profissao + "<br>";
+document.getElementById("perch-posicao").innerHTML = "Posição de aparições : " + perch.posicao + "<br>";
+
+
+document.getElementById("princesa-nome").innerHTML = "" + princesa.nome + "<br>";
+document.getElementById("princesa-genero").innerHTML = "Genero : " + princesa.genero + "<br>";
+document.getElementById("princesa-especie").innerHTML = "Espécie : " + princesa.especie + "<br>";
+document.getElementById("princesa-profissao").innerHTML = "Profissão : " + princesa.profissao + "<br>";
+document.getElementById("princesa-posicao").innerHTML = "Posição de aparições : " + princesa.posicao + "<br>";
+
+
+document.getElementById("patchy-nome").innerHTML = "" + patchy.nome + "<br>";
+document.getElementById("patchy-genero").innerHTML = "Genero : " + patchy.genero + "<br>";
+document.getElementById("patchy-especie").innerHTML = "Espécie : " + patchy.especie + "<br>";
+document.getElementById("patchy-profissao").innerHTML = "Profissão : " + patchy.profissao + "<br>";
+document.getElementById("patchy-posicao").innerHTML = "Posição de aparições : " + patchy.posicao + "<br>";
+
+document.getElementById("potty-nome").innerHTML = "" + potty.nome + "<br>";
+document.getElementById("potty-genero").innerHTML = "Genero : " + potty.genero + "<br>";
+document.getElementById("potty-especie").innerHTML = "Espécie : " + potty.especie + "<br>";
+document.getElementById("potty-profissao").innerHTML = "Profissão : " + potty.profissao + "<br>";
+document.getElementById("potty-posicao").innerHTML = "Posição de aparições : " + potty.posicao + "<br>";
+
+
+document.getElementById("karen-nome").innerHTML = "" + karen.nome + "<br>";
+document.getElementById("karen-genero").innerHTML = "Genero : " + karen.genero + "<br>";
+document.getElementById("karen-especie").innerHTML = "Espécie : " + karen.especie + "<br>";
+document.getElementById("karen-profissao").innerHTML = "Profissão : " + karen.profissao + "<br>";
+document.getElementById("karen-posicao").innerHTML = "Posição de aparições : " + karen.posicao + "<br>";
+
+
+document.getElementById("fancyson-nome").innerHTML = "" + fancyson.nome + "<br>";
+document.getElementById("fancyson-genero").innerHTML = "Genero : " + fancyson.genero + "<br>";
+document.getElementById("fancyson-especie").innerHTML = "Espécie : " + fancyson.especie + "<br>";
+document.getElementById("fancyson-profissao").innerHTML = "Profissão : " + fancyson.profissao + "<br>";
+document.getElementById("fancyson-posicao").innerHTML = "Posição de aparições : " + fancyson.posicao + "<br>";
+
+
+document.getElementById("margaret-nome").innerHTML = "" + margaret.nome + "<br>";
+document.getElementById("margaret-genero").innerHTML = "Genero : " + margaret.genero + "<br>";
+document.getElementById("margaret-especie").innerHTML = "Espécie : " + margaret.especie + "<br>";
+document.getElementById("margaret-profissao").innerHTML = "Profissão : " + margaret.profissao + "<br>";
+document.getElementById("margaret-posicao").innerHTML = "Posição de aparições : " + margaret.posicao + "<br>";
+
+
+document.getElementById("harold-nome").innerHTML = "" + harold.nome + "<br>";
+document.getElementById("harold-genero").innerHTML = "Genero : " + harold.genero + "<br>";
+document.getElementById("harold-especie").innerHTML = "Espécie : " + harold.especie + "<br>";
+document.getElementById("harold-profissao").innerHTML = "Profissão : " + harold.profissao + "<br>";
+document.getElementById("harold-posicao").innerHTML = "Posição de aparições : " + harold.posicao + "<br>";
+
+
+document.getElementById("rei-nome").innerHTML = "" + rei.nome + "<br>";
+document.getElementById("rei-genero").innerHTML = "Genero : " + rei.genero + "<br>";
+document.getElementById("rei-especie").innerHTML = "Espécie : " + rei.especie + "<br>";
+document.getElementById("rei-profissao").innerHTML = "Profissão : " + rei.profissao + "<br>";
+document.getElementById("rei-posicao").innerHTML = "Posição de aparições : " + rei.posicao + "<br>";
+
+
+document.getElementById("raio-nome").innerHTML = "" + raio.nome + "<br>";
+document.getElementById("raio-genero").innerHTML = "Genero : " + raio.genero + "<br>";
+document.getElementById("raio-especie").innerHTML = "Espécie : " + raio.especie + "<br>";
+document.getElementById("raio-profissao").innerHTML = "Profissão : " + raio.profissao + "<br>";
+document.getElementById("raio-posicao").innerHTML = "Posição de aparições : " + raio.posicao + "<br>";
